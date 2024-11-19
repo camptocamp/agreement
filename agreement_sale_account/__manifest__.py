c015c05a8b7bee39247202a1295606697a08c266
ilo <dev@nomadiplus.com>
2024-11-19 12:49:35 -0300

    [17.0][ADD] agreement_sale_account

---
+++ agreement_sale_account/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Camptocamp SA (https://www.camptocamp.com).
+# @author: Italo Lopes <italo.lopes@camptocamp.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Agreement Sale Account",
+    "summary": "Bridge between sales and account",
+    "version": "17.0.1.0.0",
+    "category": "Contract",
+    "author": "Camptocamp SA, Italo Lopes, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/agreement",
+    "license": "AGPL-3",
+    "depends": ["agreement_account", "agreement_sale"],
+    "data": [],
+    "development_status": "Beta",
+    "maintainers": ["alexis-via", "bealdav", "imlopes"],
+    "installable": True,
+}
