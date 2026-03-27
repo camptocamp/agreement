28acca468790a0c61efcc7a11633919e43af38e3
Maksym Yankin <yankinmk@gmail.com>
2026-03-27 10:23:04 +0200

    [IMP] agreement_rebate_partner_company_group: pre-commit auto fixes

--- agreement_rebate_partner_company_group/__manifest__.py
+++ agreement_rebate_partner_company_group/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "18.0.1.0.0",
     "development_status": "Beta",
     "category": "Contract",
-    "website": "https://github.com/OCA/contract",
+    "website": "https://github.com/OCA/agreement",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
